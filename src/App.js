08e0e24781209a445df6bade09968f306455bfb8
Russell Schmidt <russell.schmidt@me.com>
2021-04-10 11:56:16 -0700

    reading in placeholder data

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [coins, updateCoins] = useState([])
+  async function fetchCoins() {
+    const data = await API.get('cryptoapi', '/coins')
+    updateCoins(data.coins)
+  }
+
+  useEffect(() => {
+    fetchCoins()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -24,7 +35,15 @@ function App() {
       </header>
 
       <section>
-
+        <h2>Crypto pricing</h2>
+        {
+          coins.map((coin, index) => (
+            <div key={index}>
+              <h3>{coin.name} - {coin.symbol}</h3>
+              <h5>${coin.price_usd}</h5>
+            </div>
+          ))
+        }
       </section>
     </div>
   );
